c764b441a2632e1405ecd5f5536711e09ddeac08
Jainammodi <jainambmodi1473@gmail.com>
2022-08-08 12:36:11 +0530

    practical-1 3rd is started

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,12 +1,15 @@
+import kotlin.reflect.typeOf
+
 fun main()
 {
     println("Hello World")
 
     var x:Int = 100
     val y:Int = 200
-    val z=300.89f
+    val z=300.89
 
     println("x=$x")
     println("y=$y")
     println("z=$z")
+
 }
---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,37 @@
+fun main()
+{
+    print("Enter Enrollment number:")
+    val en = readLine()!!.toInt()
+
+    print("Enter your name:")
+    val name = readLine()!!.toString()
+
+    print("Enter your Branch:")
+    val branch = readLine()!!.toInt()
+
+    print("Enter your class:")
+    val clas = readLine()!!.toInt()
+
+    print("Enter your Batch:")
+    val batch = readLine()!!.toInt()
+
+    print("Enter your collage name:")
+    val clg = readLine()!!.toInt()
+
+    print("Enter your University name:")
+    val uni = readLine()!!.toInt()
+
+    print("Enter your age:")
+    val age = readLine()!!.toInt()
+
+    println("en = $en")
+    println("name = $name")
+    println("branch = $branch")
+    println("class = $clas")
+    println("batch = $batch")
+    println("clg = $clg")
+    println("uni = $uni")
+    println("age = $age")
+
+
+}
