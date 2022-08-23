26e5b044163bf711d7efe7baaa82d92e7217c94f
DARSHAN <darshanvasoya@gnu.ac.in>
2022-08-23 21:58:04 +0530

    Question-1

---
+++ src/main/kotlin/q-1.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    // Defining Variables :
+    val p: Int = 120
+    val q: Double = 34.546
+    val r: Float = 12.34f
+    val s: Long = 1233443556
+    val t: Short = 34
+    val u: Byte = 89
+    val v: Char = 'a'
+    val done: Boolean = false
+    val name: String = "Darshan Vasoya"
+
+    // Printing all the Variables :
+    println("Integer Value : $p")
+    println("Double Value : $q")
+    println("Float Value : $r")
+    println("Long Value : $s")
+    println("Short Value : $t")
+    println("Byte Value : $u")
+    println("Char Value : $v")
+    println("Boolean Value : $done")
+    println("String Value : $name")
+}
